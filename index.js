3b8e83390cccaaabacf3a6353bf1f4714771618c
Perminus Gaita <perminusgaita1@gmail.com>
2020-08-29 12:43:02 +0300

    docker intergration added

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node!!!\n'
   res.end(msg);
 });
 
-server.listen(port, () => {
+server.listen('80', '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
+
+/**
+ * In the context of servers, 0.0. 0.0 means all IPv4 addresses on 
+ * the local machine. If a host has two IP addresses, 192.168. ... 
+ * In the context of routing, 0.0. 0.0 usually means the default route, 
+ * i.e. the route which leads to 'the rest of' the Internet instead of 
+ * somewhere on the local network.
+ */
